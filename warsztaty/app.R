16c8fb0cb863107132c637e34ac07394386249eb
michbur <michalburdukiewicz@gmail.com>
2018-11-24 15:08:40 +0100

    added plotly

--- warsztaty/app.R
+++ warsztaty/app.R
@@ -1,5 +1,6 @@
 library(shiny)
 library(ranger)
+library(plotly)
 
 load("models.RData")
 
@@ -28,14 +29,18 @@ ui <- fluidPage(
     ),
     
     mainPanel(
-      tableOutput(outputId = "pred_table")
+      tabsetPanel(
+        tabPanel("Tabela", tableOutput(outputId = "pred_table")),
+        tabPanel("Wykres", plotlyOutput(outputId = "pred_plot"))
+      )
+      
     )
   )
 )
 
 server <- function(input, output) {
   
-  output[["pred_table"]] <- renderTable({
+  pred_df <- reactive({
     new_data <- data.frame(n_pokoj = 3,
                            metraz = 50,
                            pietro = 1,
@@ -48,6 +53,18 @@ server <- function(input, output) {
     
     data.frame(new_data, cena_m2 = predict(model_best, new_data)[["predictions"]])
   })
+  
+  output[["pred_table"]] <- renderTable({
+    pred_df()
+  })
+  
+  output[["pred_plot"]] <- renderPlotly({
+    ggplotly(ggplot(pred_df(), aes(x = rok, y = cena_m2, color = dzielnica)) +
+      geom_point() +
+      geom_line() +
+      theme_bw())
+  })
+  
 }
 
 shinyApp(ui = ui, server = server)
